946dfbd7f1a8e82735523844f5251c7f6e479357
Liang Zhang <psychelzh@outlook.com>
2021-04-14 16:13:03 +0800

    🐞fix: update workflow
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _targets.R
+++ _targets.R
@@ -1,35 +1,38 @@
-# load required packages used in pipeline setup
-options(tidyverse.quiet = TRUE)
 library(targets)
 library(tarchetypes)
-library(tidyverse)
 future::plan(future::multisession)
-# set required packages used in pipeline running
-tar_option_set(packages = "tidyverse")
-# prepare configurations for games to be fetched
-games <- tarflow.iquizoo::fetch("sql/games.tmpl.sql", config::get("where")) %>%
-  left_join(dataproc.iquizoo::game_info, by = "game_id") %>%
-  mutate(prep_fun = syms(prep_fun_name))
-# add more jobs in the following plans
-list(
-  tar_file(file_config, "config.yml"),
-  tar_file(query_tmpl_data, "sql/data.tmpl.sql"),
-  tar_file(query_tmpl_users, "sql/users.tmpl.sql"),
-  tar_target(config_where, config::get("where", file = file_config)),
-  tar_fst_tbl(users, tarflow.iquizoo::fetch(query_tmpl_users, config_where)),
-  targets_data <- tar_map(
-    values = games,
-    names = game_name_abbr,
-    tar_target(
-      data,
-      tarflow.iquizoo::fetch(
-        query_tmpl_data,
-        tarflow.iquizoo::insert_where(
-          config_where,
-          list(table = "content", field = "Id", values = game_id)
-        )
+search_games_mem <- memoise::memoise(
+  tarflow.iquizoo::search_games,
+  cache = cachem::cache_disk("~/.cache.tarflow.iquizoo")
+)
+games <- search_games_mem(config::get("where"))
+tar_option_set(package = c("tidyverse", "dataproc.iquizoo"), imports = "dataproc.iquizoo")
+targets_data <- tar_map(
+  values = games,
+  names = game_name_abbr,
+  tar_target(
+    data,
+    tarflow.iquizoo::fetch(
+      query_tmpl_data,
+      tarflow.iquizoo::insert_where(
+        config_where,
+        list(table = "content", field = "Id", values = game_id)
       )
     )
   ),
-  tar_combine(data, targets_data)
+  tar_target(
+    indices,
+    tarflow.iquizoo::calc_indices(data, prep_fun)
+  )
+)
+list(
+  tar_file(file_config, "config.yml"),
+  tar_target(config_where, config::get("where", file = file_config)),
+  tar_file(query_tmpl_users, fs::path("sql", "users.tmpl.sql")),
+  tar_fst_tbl(users, tarflow.iquizoo::fetch(query_tmpl_users, config_where)),
+  tar_file(query_tmpl_data, fs::path("sql", "data.tmpl.sql")),
+  tar_file(query_tmpl_games, fs::path("sql", "games.tmpl.sql")),
+  targets_data,
+  tar_combine(data, targets_data[[1]]),
+  tar_combine(indices, targets_data[[2]])
 )
