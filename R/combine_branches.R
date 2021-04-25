8cec1f762308ec3fdb5693477aae1c083a9a64c0
Liang Zhang <psychelzh@outlook.com>
2021-04-25 23:33:34 +0800

    🐞fix: add game info
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/combine_branches.R
+++ R/combine_branches.R
@@ -10,11 +10,11 @@
 combine_branches <- function(targets, stack = TRUE, ...) {
   # merge meta and data then join branches
   targets %>%
-    map(wrangle_meta, stack = stack, ...) %>%
+    imap(wrangle_meta, stack = stack, ...) %>%
     bind_rows()
 }
 
-wrangle_meta <- function(data, stack, ...) {
+wrangle_meta <- function(data, name, stack, ...) {
   if (is.null(data)) {
     return(NULL)
   }
@@ -22,11 +22,12 @@ wrangle_meta <- function(data, stack, ...) {
   name_key <- attr(data, "name_key")
   data_cols <- setdiff(names(data), name_key)
   data_and_meta <- data %>%
+    add_column(game_name_abbr = str_extract(name, "(?<=_)[^_]+$")) %>%
     left_join(meta, by = name_key) %>%
-    group_by(user_id, game_id) %>%
+    group_by(user_id, game_id, ) %>%
     mutate(times = row_number(game_time)) %>%
     ungroup() %>%
-    select(user_id, times, all_of(data_cols))
+    select(user_id, game_id, game_name_abbr, times, all_of(data_cols))
   if (stack) {
     data_and_meta <- data_and_meta %>%
       pivot_longer(all_of(data_cols), ...)
