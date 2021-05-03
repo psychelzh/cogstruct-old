d48e099cd2636b25e08ef8e9e262b645fca92c15
Liang Zhang <psychelzh@outlook.com>
2021-05-03 23:12:40 +0800

    🦄 refactor(efa): shorten names
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/prep_efa_dataset.R
+++ R/prep_efa_dataset.R
@@ -2,12 +2,12 @@
 #'
 #' @title
 #' @param indices_clean
-#' @param config_selection
+#' @param config_sel
 #' @param rm
 #' @return
 #' @author Liang Zhang
 #' @export
-prep_efa_dataset <- function(indices_clean, config_selection, rm = NULL) {
+prep_efa_dataset <- function(indices_clean, config_sel, rm = NULL) {
   if (is.null(rm)) {
     rm <- "outlier"
   }
@@ -19,7 +19,7 @@ prep_efa_dataset <- function(indices_clean, config_selection, rm = NULL) {
     outlier = indices_clean %>%
       filter(!is_outlier)
   )
-  config_selection %>%
+  config_sel %>%
     filter(!type %in% c("exclude", "outcome")) %>%
     left_join(indices_used, by = c("game_id", "index")) %>%
     filter(occasion == 1) %>%
