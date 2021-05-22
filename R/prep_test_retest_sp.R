bf03e6d768190c7f7f99ca4aa0eb606a0937c0e3
Liang Zhang <psychelzh@outlook.com>
2021-05-22 16:39:52 +0800

    🦄 refactor: enhance code quality
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/prep_test_retest_sp.R
+++ R/prep_test_retest_sp.R
@@ -8,15 +8,20 @@
 #' @author Liang Zhang
 #' @export
 prep_test_retest_sp <- function(indices_clean) {
-  games_a <- c("BPSA", "DRIA", "NVRIA", "NVRDA", "FOLDA", "DRM", "MRC", "MR3DA", "RavenSE")
-  games_b <- c("BPSB", "DRIB", "NVRIB", "NVRDB", "FOLDB", "DRMB", "MRD", "MR3DB", "RavenSO")
-  games_id_a <- with(game_info, game_id[game_name_abbr %in% games_a])
-  games_id_b <- with(game_info, game_id[game_name_abbr %in% games_b])
-  recode_ids <- set_names(games_id_a, games_id_b)
+  recode_ids <- tibble(
+    name_from = c("BPSB", "DRIB", "NVRIB", "NVRDB", "FOLDB", "DRMB", "MRD", "MR3DB", "RavenSO"),
+    name_to = c("BPSA", "DRIA", "NVRIA", "NVRDA", "FOLDA", "DRM", "MRC", "MR3DA", "RavenSE")
+  ) %>%
+    rowwise() %>%
+    mutate(across(
+      .fns = ~ with(game_info, game_id[game_name_abbr == .x])
+    )) %>%
+    deframe()
   indices_clean %>%
-    filter(game_id %in% c(games_id_a, games_id_b)) %>%
+    filter(game_id %in% c(recode_ids, names(recode_ids))) %>%
     mutate(
-      occasion = ifelse(game_id %in% games_id_b, 2, occasion),
+      occasion = ifelse(game_id %in% names(recode_ids), 2, occasion),
       game_id = recode(game_id, !!!recode_ids)
-    )
+    ) %>%
+    prep_test_retest()
 }
